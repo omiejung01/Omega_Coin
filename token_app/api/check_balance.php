a02d3cfc507bd8f651929384a217eadda8ecb9ac
Dhirachat Chayaporn <omiejung@gmail.com>
2025-06-21 16:07:05 +0700

    Omega coin PHP-17  add remarks in account_balance - on progress

--- token_app/api/check_balance.php
+++ token_app/api/check_balance.php
@@ -18,7 +18,7 @@ $output = ["result" => "Error, No account"];
 
 if (is_existed($account_id, $realm_id, $conn)) {
 	$balance = account_balance($account_id,$realm_id, $conn, $account_name, $account_type, $account_remarks);
-	$output = ["realm_id" => $realm_id ,"account_id" => $account_id,"account_name" =>$account_name, "account_type" => $account_type,"balance" => $balance];
+	$output = ["realm_id" => $realm_id ,"account_id" => $account_id,"account_name" =>$account_name, "account_type" => $account_type, "account_remarks" => $account_remarks, "balance" => $balance];
 	
 }
 
