65ce9694e9c7a3ea499fe569802e83b916c877c6
Dhirachat Chayaporn <omiejung@gmail.com>
2025-07-12 13:59:27 +0700

    Omega coin Give-away - 06 - on progress

--- token_app/api/give_away.php
+++ token_app/api/give_away.php
@@ -4,6 +4,8 @@ require("account.php");
 error_reporting(E_ALL);
 header("Content-Type: application/json");
 
+$realm_id = trim(htmlspecialchars($_GET["realm_id"]));
+
 class ThisAccount {
   public $account_id;
   public $total;
@@ -53,10 +55,11 @@ $list_result2 = array();
 $list_result3 = array();
 $list_result4 = array();
 	
-$sql3 = "SELECT account_id, account_type, remarks FROM account WHERE void = 0 ORDER by account_id ";
+$sql3 = "SELECT account_id, account_type, remarks FROM account WHERE void = 0 AND realm_id = ? ORDER by account_id ";
 $found = false; 
 
 if ($stmt3 = $conn->prepare($sql3)) {
+	$stmt3->bind_param("s", $realm_id);
 	$stmt3->execute();
 	$result3 = $stmt3->get_result();
 	while ($row3 = $result3->fetch_assoc()) {
@@ -72,10 +75,11 @@ if ($stmt3 = $conn->prepare($sql3)) {
 //  echo "$x->account_id $x->account_type $x->remarks \n";
 //}
 
-$sql4 = "SELECT to_account, sum(amount) AS total FROM transfer WHERE transfer.void = 0 GROUP BY from_account ";
+$sql4 = "SELECT to_account, sum(amount) AS total FROM transfer WHERE transfer.void = 0 AND realm_id = ? GROUP BY from_account ";
 $found = false; 
 
 if ($stmt4 = $conn->prepare($sql4)) {
+	$stmt4->bind_param("s", $realm_id);
 	$stmt4->execute();
 	$result4 = $stmt4->get_result();
 	while ($row4 = $result4->fetch_assoc()) {
@@ -91,10 +95,11 @@ if ($stmt4 = $conn->prepare($sql4)) {
 //  echo "$y->account_id " . "$y->total \n";
 //}
 
-$sql5 = "SELECT from_account, sum(amount) AS total FROM transfer WHERE transfer.void = 0 GROUP BY from_account ";
+$sql5 = "SELECT from_account, sum(amount) AS total FROM transfer WHERE transfer.void = 0 AND realm_id = ? GROUP BY from_account ";
 $found = false; 
 
 if ($stmt5 = $conn->prepare($sql5)) {
+	$stmt5->bind_param("s", $realm_id);
 	$stmt5->execute();
 	$result5 = $stmt5->get_result();
 	while ($row5 = $result5->fetch_assoc()) {
@@ -136,8 +141,55 @@ foreach ($list_result as $z) {
 	$list_result4[] = new ThisAccount2($z->account_id,$z->account_type,$z->remarks, $to_amount, $from_amount, $balance);
 }
 
+$output = "";
+
 foreach ($list_result4 as $c) {
-  echo "$c->account_id $c->account_type $c->remarks $c->to_amount $c->from_amount $c->balance \n";
+  $acc_id = $c->account_id;
+  $acc_type = $c->account_type;
+  $acc_remark = $c->remarks;
+  $acc_balance = -($c->balance);
+  if (strcmp($acc_type,"Liabilities")==0 && strcmp($acc_remark,"No")==0) {
+	if ($acc_balance < 200) {
+		//echo "$c->account_id $c->account_type $c->remarks $c->to_amount $c->from_amount $c->balance \n";
+		
+		// generate new ID 
+		$sql6 = "SELECT MAX(transfer_id) AS max_id FROM transfer ";
+		$stmt6 = $conn->query($sql6);
+		$id = 0;
+		
+		if ($stmt6 && $stmt6->num_rows > 0) {
+			$row6 = $stmt6->fetch_assoc();
+			if ($row6['max_id'] !== null) { 
+				$id = $row6['max_id'];
+			}
+			$stmt6->free();
+		} else {
+			
+			$output = ["result" => "Error, Cannot transfer"];
+		}		
+
+		$id += 1;	
+		$stmt6->close();
+		
+		$sql2 = "INSERT INTO transfer (transfer_id, from_account, to_account, amount, remarks, realm_id, ip_address) VALUES (?, ?, ?, ?, ?, ?, ?)";
+
+		/*
+		$stmt = $conn->prepare($sql2);
+		if ($stmt === FALSE) {
+			$output = ["result" =>  ("Error 1 " . $conn->error )];
+		} else {
+			$stmt->bind_param("ississs", $id, $from_account, $to_account, $amount, $remarks, $realm_id, $_SERVER['REMOTE_ADDR']);
+			if ($stmt->execute()) {
+				$output = ["transfer_id" => $id, "remarks" => $remarks, "result" => "Success"];
+			} else {
+				$output = ["result" =>  ("Error 2 " . $conn->error) , "id" => $id];
+			}
+			$stmt->close();
+		}
+		*/
+		
+	}
+  }
 }
 
 
